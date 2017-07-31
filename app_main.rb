665eafe61c3c3002cd24b3486400870c990ded45
Takamasa Mizukami <mizukami2005@gmail.com>
2017-07-31 09:33:31 +0900

    カルーセルの追加

--- app_main.rb
+++ app_main.rb
@@ -52,8 +52,62 @@ post '/callback' do
                      ]
           }
         }
+        carousel = {
+          type:     "template",
+          altText:  "this is a carousel template",
+          template: {
+            type:    "carousel",
+            columns: [
+                       {
+                         thumbnailImageUrl: "https://example.com/bot/images/item1.jpg",
+                         title:             "this is menu",
+                         text:              "description",
+                         actions:           [
+                                              {
+                                                type:  "postback",
+                                                label: "Buy",
+                                                data:  "action=buy&itemid=111"
+                                              },
+                                              {
+                                                type:  "postback",
+                                                label: "Add to cart",
+                                                data:  "action=add&itemid=111"
+                                              },
+                                              {
+                                                type:  "uri",
+                                                label: "View detail",
+                                                uri:   "http://example.com/page/111"
+                                              }
+                                            ]
+                       },
+                       {
+                         thumbnailImageUrl: "https://example.com/bot/images/item2.jpg",
+                         title:             "this is menu",
+                         text:              "description",
+                         actions:           [
+                                              {
+                                                type:  "postback",
+                                                label: "Buy",
+                                                data:  "action=buy&itemid=222"
+                                              },
+                                              {
+                                                type:  "postback",
+                                                label: "Add to cart",
+                                                data:  "action=add&itemid=222"
+                                              },
+                                              {
+                                                type:  "uri",
+                                                label: "View detail",
+                                                uri:   "http://example.com/page/222"
+                                              }
+                                            ]
+                       }
+                     ]
+          }
+        }
+
         if event.message['text'] == '確認'
-          client.reply_message(event['replyToken'], question)
+          client.reply_message(event['replyToken'], carousel)
         else
           client.reply_message(event['replyToken'], message)
         end
